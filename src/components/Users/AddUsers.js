76273d75c27d3617fb130e23b620e2ece1165f8d
babyazalea <usun12@hotmail.com>
2021-07-03 01:03:11 +0900

    using fragment & createPortal

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import Wrapper from "../Helpers/Wrapper";
 import ErrorModal from "../UI/ErrorModal";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -45,7 +46,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -72,7 +73,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
